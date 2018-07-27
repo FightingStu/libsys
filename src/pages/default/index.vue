e2e457a66fb5e1e6410d14dbe65d5ed09d9ea963
wilsunson <839785143@qq.com>
2018-07-27 09:57:36 +0800

    Initial commit

---
+++ src/pages/default/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="contain">
+        <div class="logo">
+            <img src="../../image/logo.png"><!-- 取上两级（../../）目录下的logo图 -->
+        </div>
+    </div>
+</template>
+
+
+<style>
+.contain{
+    width: 100%;
+    background-color: #fefefe;
+}
+
+.logo {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 30%;
+}
+</style>
